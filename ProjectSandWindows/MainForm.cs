9eaa3c49309380ee9c5c1db085255e6393a2e95d
dhavatar <dhavatar@b0af51f8-88b6-11dd-8cff-af8a609d5e3c>
2008-09-23 02:29:29 +0000

    Initial import

---
+++ ProjectSandWindows/MainForm.cs
@@ -0,0 +1,28 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// MainForm.cs
+//
+// Copyright (C) Project Sand
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+#endregion
+
+namespace ProjectSandWindows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
